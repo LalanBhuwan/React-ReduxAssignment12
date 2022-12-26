90dbcd9bc56124af1a2bbf4d2cff2323ce647ccc
LalanBhuwan <bhuwanlalan475@gmail.com>
2022-12-26 21:36:29 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import Service from "./components/Service";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function App() {
+  const msg = useSelector((state) => state.msg);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <BrowserRouter>
+      <div>{msg}</div>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Service" element={<Service />} />
+      </Routes>
+     
+    </BrowserRouter>
+    
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import {configureStore} from '@reduxjs/toolkit';
+import defaultReducer from './utils';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const stateStore = configureStore({
+  redicer:defaultReducer
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={stateStore}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
